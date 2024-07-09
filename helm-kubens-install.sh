d4011e505a4cffcb953e500fc0d3e6462ed28820
surya pelluru <maladikrishnakishore@gmail.com>
2024-07-10 01:09:32 +0530

    eks

---
+++ helm-kubens-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#helm install
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#kubens install
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
